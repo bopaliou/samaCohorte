1b0d9d96adcd2999f6418ff6de2e67e76a492f1f
Aliou Bop <bopaliou1994@gmail.com>
2021-05-16 22:52:52 +0000

    Ajout d'une classe Developpeur héritant de la class Collaborateur

--- file2.php
+++ file2.php
@@ -11,7 +11,36 @@ class Collaborateur{
         echo 'Bonjour '.$this->prenom.' '.$this->nom.' Vous venez de '.$this->$adresse.'<br/>';
     }
 }
+Developpeur extends Collaborateur{
+    public $type_dev="back-end";
+    public function backendDev(){
+        parent::direBonjour();
+        echo "Et vous etes developpeur ".$type_dev;
+    }
+    parent::direBonjour();
+    echo "Et vous etes developpeur ".$type_dev;
+
+}
+//Aliou
+
 $aliou=new Collaborateur("BOP","Aliou","Fass");
+$aliou_dev=new Developpeur();
+$aliou_dev->backendDev();
+
+//Yussuf
+
 $yussuf=new= Collaborateur("DIONE","Yussuf","Colobanne");
+$yussuf_dev=new Developpeur();
+$yussuf_dev->backendDev();
+
+//Baba
+
 $baba=new Collaborateur("SAMB","Baba","Mermoz");
+$baba_dev=new Developpeur();
+$baba_dev->backendDev();
+
+//Anna
+
 $anna=new Collaborateur("DIONE","Anna","Parcelles Asssainies");
+$anna_dev=new Developpeur();
+$anna_dev->backendDev();
