e067601612c2f969c262b10c6f17c5fbe97e15f6
Aliou Bop <bopaliou1994@gmail.com>
2021-05-17 02:15:15 +0000

    Ajout d'une nouvelle sur file1

--- file1.php
+++ file1.php
@@ -3,5 +3,6 @@
     foreach ($collaborateurs as $collaborateur) {
         echo "Bonjour ".$collaborateur.'<br/>';
         echo "Bienvenue ".$collaborateur." sur le projet de la veille<br/>";
+        echo "Vous pouvez commencer à travailler";
     }
 ?>
