e41efe0ac46f9afc632c6ee8d6b9c752eb8de859
Aliou Bop <bopaliou1994@gmail.com>
2021-05-17 02:06:28 +0000

    Ajout d'une fonction pour la soustraction de deux(2) nombres

--- file3.php
+++ file3.php
@@ -9,8 +9,13 @@ function multiplication($a,$b){
     $produit=$a*$b;
     echo $a.' x '.$b.' = '.$produit;
 }
+function soustraction($a,$b){
+    $soustraction=$a-$b;
+    echo $a.' - '.$b.' = '.$soustraction;
+}
 addition(15,20);
 multiplication(5,9);
+soustraction(10,8);
 
 
 ?>
