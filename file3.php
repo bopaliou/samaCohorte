fc68bf1941ab835d157363b5eedf94a5cb74a64c
Aliou Bop <bopaliou1994@gmail.com>
2021-05-17 02:00:21 +0000

    Ajout d'une fonction pour la multiplication de deux(2) nombres

--- file3.php
+++ file3.php
@@ -5,7 +5,12 @@ function addition($a,$b){
     echo $a.' + '.$b.' = '.$result;
 
 }
+function multiplication($a,$b){
+    $produit=$a*$b;
+    echo $a.' x '.$b.' = '.$produit;
+}
 addition(15,20);
+multiplication(5,9);
 
 
 ?>
